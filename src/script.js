bf64664c1006d40ec83882efe7c54048bd37dc6b
Federico Tensi <federicotensi@gmail.com>
2024-02-15 20:07:15 +0100

    new workflow

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
+const github = require("@actions/github");
 const fs = require("fs");
-const path = require("path");
 
 const contextJson = fs.readFileSync("./context.json", "utf8");
 const context = JSON.parse(contextJson);
@@ -20,28 +20,26 @@ if (isClosed && hasLabel && hasSetupTag) {
 	console.log(carName, trackName);
 	const username = issue.user.login.trim();
 	const approvedBy = context.sender.login.trim();
+	const setupBlob = issue.body.match(/```ini\n([\s\S]+)```/)[1].trim();
 	const setupFilePath = `./setups/${carName}/${trackName}/${username}.ini`;
-	const dirPath = path.dirname(setupFilePath);
 
-	if (!fs.existsSync(dirPath)) {
-		console.log(
-			`Creating setup file for ${carName} at ${trackName} from ${username} approved by ${approvedBy} dirpath: ${dirPath}, path: ${setupFilePath}, ${process.cwd()}`,
-		);
-		try {
-			fs.mkdirSync(dirPath, {
-				recursive: true,
-			});
-		} catch (err) {
-			console.error(err);
-		}
+	const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+	const commitMessage = `Create setup file for ${carName} at ${trackName} from ${username} approved by ${approvedBy}`;
+	const commitContent = Buffer.from(setupBlob).toString("base64");
 
-		try {
-			fs.writeFileSync(setupFilePath, "Hello test");
-		} catch (err) {
-			console.error(err);
-		}
-		// fs.writeFileSync(setupFilePath, "");
-	} else {
-		console.log(`Setup file for ${carName} at ${trackName} from ${username} already exists`);
-	}
+	octokit.repos
+		.createOrUpdateFile({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			path: setupFilePath,
+			message: commitMessage,
+			content: Buffer.from(commitContent).toString("base64"),
+			branch: github.context.ref.replace("refs/heads/", ""),
+		})
+		.then((resopnse) => {
+			console.log("Commit created: ", response.data.commit.sha);
+		})
+		.catch((error) => {
+			console.error("Error creating commit: ", error);
+		});
 }
